cfedb24c8c03dcbc689c476d4bd75695dcceaecb
ATG-Tamim <atg.ahmad.tamim.ghyasi@gmail.com>
2021-01-11 11:03:21 +0430

    Implement header section

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="header">
+    <div class="logo">
+      <!-- <img src="./../assets/images/logo.svg" alt="startGlobal"> -->
+    </div>
+    <div class="header-links">
+      <div class="active">
+        <div>Personal Plans</div>
+        <div class="active-line"></div>
+      </div>
+      <div>Business Plans</div>
+      <div>Guide</div>
+      <div>Blog</div>
+    </div>
+    <div class="profile">
+      <div class="basket-icon">
+        <img src="./../assets/font-icons/basket.svg" alt />
+      </div>
+      <div class="profile-options">
+        <div class="profile-pic">AT</div>
+        <div class="profile-name">
+          Tamim
+          <img src="./../assets/font-icons/arrow-down.svg" alt />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
