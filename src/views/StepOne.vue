4f433a7ea693457e30e7bf3b860e0f03e4babc8e
ATG-Tamim <atg.ahmad.tamim.ghyasi@gmail.com>
2021-01-14 10:54:22 +0430

    Add animation to input

--- src/views/StepOne.vue
+++ src/views/StepOne.vue
@@ -7,8 +7,8 @@
         class="steps-para form-item"
       >It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
       <div class="input-form form-item">
-        <input type="text" placeholder="ZIP code" @click="transition"/>
-        <button type="submit" @click="addNum">
+          <input ref="zipCodeInp" class="input-disable" type="text" placeholder="ZIP code" v-model="inputValue"/>
+          <button ref="zipCodeBtn" class="btn-disable">
           Next
           <span>
             <img src="./../assets/font-icons/arrow-left.svg" alt />
@@ -23,16 +23,30 @@
 </template>
 
 <script>
+
 export default {
   data() {
-    return {};
+    return {
+      inputValue: ''
+    };
   },
 
   methods: {
-    addNum() {
-      this.$emit("addNum");
-    },
+    nextStep() {
+    }
   },
+
+  watch: {
+    inputValue: function(value) {
+      if (this.inputValue.length >= 4) {
+        this.$refs.zipCodeInp.classList.remove('input-disable')
+        this.$refs.zipCodeBtn.classList.remove('btn-disable')
+      } else {
+        this.$refs.zipCodeInp.classList.add('input-disable')
+        this.$refs.zipCodeBtn.classList.add('btn-disable')
+      }
+    }
+  }
 };
 </script>
 
