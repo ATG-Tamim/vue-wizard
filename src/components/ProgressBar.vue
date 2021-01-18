6cbd0423254f7c94651de949ae3deb9dfc8e3c0f
ATG-Tamim <atg.ahmad.tamim.ghyasi@gmail.com>
2021-01-18 10:00:07 +0430

    Fix some design issue on progressBar

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -5,27 +5,27 @@
       <div class="active" ref="activeLine"></div>
     </div>
       <div class="step step-one active-circle" @click="stepOne">
-        <div class="inline-circle" ref="activeStepOne">
+        <div class="inline-circle" :class="visitedSteps.includes(1) && 'active'">
           1
         </div>
       </div>
     <div class="step step-two" @click="stepTwo">
-      <div class="inline-circle" ref="activeStepTwo">
+      <div class="inline-circle" :class="visitedSteps.includes(2) && 'active'">
         2
       </div>
     </div>
     <div class="step step-three" @click="stepThree">
-      <div class="inline-circle" ref="activeStepThree">
+      <div class="inline-circle" :class="visitedSteps.includes(3) && 'active'">
         3
       </div>
     </div>
     <div class="step step-four" @click="stepFour">
-      <div class="inline-circle" ref="activeStepFour">
+      <div class="inline-circle" :class="visitedSteps.includes(4) && 'active'">
         4
       </div>
     </div>
     <div class="step step-five" @click="stepFive">
-        <div class="inline-circle" ref="activeStepFive">5</div>
+        <div class="inline-circle" :class="visitedSteps.includes(5) && 'active'">5</div>
     </div>
   </div>
 </template>
@@ -35,12 +35,12 @@ export default {
 
   data() {
     return {
-      progressLine: 1,
-      stepTwoClick: false,
-      stepThreeClick: false,
-      stepFourClick: false,
-      stepFiveClick: false,
+      visitedSteps: []
     }
+  },
+
+  created() {
+    this.visitedSteps = JSON.parse(localStorage.getItem('visitedSteps')) || [];
   },
 
   methods: {
@@ -48,75 +48,67 @@ export default {
       this.$router.push('/')
     },
     stepTwo() {
-      if(this.stepTwoClick) {
-        this.$router.push('/step-two')
-      }
+      if (! this.visitedSteps.includes(2)) return;
+      this.$router.push('/step-two')
     },
     stepThree() {
-      if(this.stepThreeClick) {
-        this.$router.push('/step-three')
-      }
+      if (! this.visitedSteps.includes(3)) return;
+      this.$router.push('/step-three')
     },
     stepFour() {
-      if(this.stepFourClick) {
-        this.$router.push('/step-four')
-      }
+      // if (! this.visitedSteps.includes(4)) return;
+      this.$router.push('/step-four')
     },
     stepFive() {
-      if(this.stepFiveClick) {
-        this.$router.push('/step-five')
+      // if (! this.visitedSteps.includes(5)) return/\\;
+      this.$router.push('/step-five')
+    },
+
+    progressResult(number) {
+      if (number == '1') {
+          return number * (3 * (2 + 3))
+        } else if (number == '2') {
+          return number * (8 + 7) + 9
+        } else if (number == '3') {
+          return number * 30 - 27
+        } else if (number == '4') {
+          return number * 20 + 5
+        } else if (number == '5') {
+          return number * 20
+        }
       }
-    }
   },
 
   watch : {
     $route(route) {
       if (route.name == 'StepOne') {
+        this.visitedSteps = [...new Set([...this.visitedSteps, 1])];
+        // this.$refs.activeLine.style.height = '15%'
         this.$refs.outlineCircle.style.transform = 'translate(0, -6px)'
-        if (this.progressLine == 1) {
-          this.$refs.activeLine.style.height = '15%'
-        }
-        this.progressLine = this.progressLine + 1
-        this.$refs.activeStepOne.classList.add('active')
       }
       if (route.name == 'StepTwo') {
-        this.$refs.outlineCircle.style.transform = 'translate(0, 106px)'
-        if (this.progressLine == 2) {
-          this.$refs.activeLine.style.height = '39%'
-        }
-        this.progressLine = this.progressLine + 1
-        this.$refs.activeStepTwo.classList.add('active')
-        this.stepTwoClick = true
+        this.visitedSteps = [...new Set([...this.visitedSteps, 1, 2])];
+        // this.$refs.activeLine.style.height = '39%'
+        this.$refs.outlineCircle.style.transform = 'translate(0, 106px)';
       }
       if (route.name == 'StepThree') {
+        this.visitedSteps = [...new Set([...this.visitedSteps, 1, 2, 3])];
+        // this.$refs.activeLine.style.height = '63%'
         this.$refs.outlineCircle.style.transform = 'translate(0, 218px)'
-        if (this.progressLine == 3) {
-          this.$refs.activeLine.style.height = '63%'
-        }
-        this.progressLine = this.progressLine + 1
-        this.$refs.activeStepThree.classList.add('active')
-        this.stepThreeClick = true
-
       }
       if (route.name == 'StepFour') {
+        this.visitedSteps = [...new Set([...this.visitedSteps, 1, 2, 3, 4])];
+        // this.$refs.activeLine.style.height = '85%'
         this.$refs.outlineCircle.style.transform = 'translate(0, 330px)'
-        if (this.progressLine == 4) {
-          this.$refs.activeLine.style.height = '86%'
-        }
-        this.progressLine = this.progressLine + 1
-        this.$refs.activeStepFour.classList.add('active')
-        this.stepFourClick = true
       }
       if (route.name == 'StepFive') {
+        this.visitedSteps = [...new Set([...this.visitedSteps, 1, 2, 3, 4, 5])];
+        // this.$refs.activeLine.style.height = '100%'
         this.$refs.outlineCircle.style.transform = 'translate(0, 442px)'
-        if (this.progressLine == 5) {
-          this.$refs.activeLine.style.height = '100%'
-        }
-        this.progressLine = this.progressLine + 1
-        this.$refs.activeLine.style.height = '100%'
-        this.$refs.activeStepFive.classList.add('active')
-        this.stepFiveClick = true
       }
+
+      localStorage.setItem('visitedSteps', JSON.stringify(this.visitedSteps));
+      this.$refs.activeLine.style.height = (this.progressResult(this.visitedSteps.length)) + '%'
     }
   },
 };
