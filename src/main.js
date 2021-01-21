f098f2eb70c1f8e7d177f0605accb43eaef6137a
ATG-Tamim <atg.ahmad.tamim.ghyasi@gmail.com>
2021-01-21 11:07:03 +0430

    Link bootstrap4 files

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/style/style.scss'
+import './assets/Bootstrap4/css/bootstrap.css'
+import './assets/Bootstrap4/js/bootstrap'
 
 createApp(App).use(router).mount('#app')
