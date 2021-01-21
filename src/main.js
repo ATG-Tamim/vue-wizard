c1574bbac657b85ccab9525fb1757a8f4bf7aad0
ATG-Tamim <atg.ahmad.tamim.ghyasi@gmail.com>
2021-01-21 11:11:10 +0430

    Remove bootstrap4 files

--- src/main.js
+++ src/main.js
@@ -2,7 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/style/style.scss'
-import './assets/Bootstrap4/css/bootstrap.css'
-import './assets/Bootstrap4/js/bootstrap'
 
 createApp(App).use(router).mount('#app')
